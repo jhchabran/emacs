314395c7a4c2c256c7dccc410b3f6c48c6973432
Jean-Hadrien Chabran <jh@chabran.fr>
2011-03-01 01:37:26 +0100

    Move Emacs temp files to /tmp

--- me/global.el
+++ me/global.el
@@ -3,10 +3,16 @@
 (push "/opt/local/bin" exec-path)
 
 ;; We've got git for that
-(setq make-backup-files nil)
+;; (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq-default tab-width 2)
 (setq vc-handled-backends nil)
+
+;; Don't clutter my source tree 
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; Use spaces to instead of tabs
 (setq-default indent-tabs-mode nil)
